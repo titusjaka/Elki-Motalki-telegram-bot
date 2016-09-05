3cd437185054f733072d05fd0a3b2cdc8318eb35
Denis Titusov <titusjaka@skbkontur.ru>
2016-09-05 14:13:43 +0500

    Done #1: added bot analytics via botan.io

--- src/bot.py
+++ src/bot.py
@@ -5,19 +5,26 @@ import os
 import telebot
 import time
 from schedule import get_schedule
+import botan
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+BOTAN_TOKEN = os.getenv('BOTAN_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
 start_message = """Привет, дружок! Я могу рассказать тебе о ближайших турах Елок Моталок. Если тебе интересно, напиши "туры".
 Я пока что совсем маленький и многого не умею, но я учусь!"""
 
 
-def pretify_schdeule(schedule):
+def pretify_schdeule(schedule, user_uuid=None):
     schedule_list = []
     for tour in schedule:
-        tour_string = ("Тур: {name}, едем {date}.\n{description}").format(**tour)
-        schedule_list.append(tour_string)
+        try:
+            tour["shorten_link"] = botan.shorten_url(tour["description"], BOTAN_TOKEN, user_uuid.id)
+            tour_string = ("Тур: {name}, едем {date}.\n{shorten_link}").format(**tour)
+        except:
+            tour_string = ("Тур: {name}, едем {date}.\n{description}").format(**tour)
+        finally:
+            schedule_list.append(tour_string)
     return schedule_list
 
 @bot.message_handler(commands=["start", "help"])
@@ -25,22 +32,23 @@ def start_and_help(message):
     response_markup = telebot.types.ReplyKeyboardMarkup()
     response_markup.add("Туры")
     bot.send_message(message.chat.id, start_message, reply_markup=response_markup)
+    botan.track(BOTAN_TOKEN, message.from_user, message, "Start")
 
 
 @bot.message_handler(regexp=".*(?i)(туры|tour).*")
 def send_tour_info(message):
     hider = telebot.types.ReplyKeyboardHide()
     bot.send_message(message.chat.id, 'Сейчас подумаю...', reply_markup=hider)
+    botan.track(BOTAN_TOKEN, message.from_user, message, "Tours")
     schedule = get_schedule()
     if not schedule:
         bot.send_message(message.chat.id, "Кажется, какой-то жулик украл все наше расписание. Но мы его уже ищем!")
         return
-    pretty_messages = pretify_schdeule(schedule)
+    pretty_messages = pretify_schdeule(schedule, message.from_user)
     bot.send_message(message.chat.id, "Наши ближайшие туры:\n====================")
     for sch in pretty_messages:
         bot.send_message(message.chat.id, '{0}\n----'.format(sch))
         time.sleep(2)
-
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
---
+++ src/botan.py
@@ -0,0 +1,60 @@
+# ----------------
+# dont forget 'pip install requests' first
+# ----------------
+# usage example:
+#
+# import botan
+#
+# print botan.track(1111, 1, {'text':2}, 'Search')
+
+import requests
+import json
+
+
+TRACK_URL = 'https://api.botan.io/track'
+SHORTENER_URL = 'https://api.botan.io/s/'
+
+
+def make_json(message):
+    data = {}
+    data['message_id'] = message.message_id
+    data['from'] = {}
+    data['from']['id'] = message.from_user.id
+    if message.from_user.username is not None:
+        data['from']['username'] = message.from_user.username
+    data['chat'] = {}
+    # Chat.Id используется в обоих типах чатов
+    data['chat']['id'] = message.chat.id
+    return data
+
+
+def track(token, uid, message, name='Message'):
+    try:
+        r = requests.post(
+            TRACK_URL,
+            params={"token": token, "uid": uid, "name": name},
+            data=json.dumps(make_json(message)),
+            headers={'Content-type': 'application/json'},
+        )
+        return r.json()
+    except requests.exceptions.Timeout:
+        # set up for a retry, or continue in a retry loop
+        return False
+    except (requests.exceptions.RequestException, ValueError) as e:
+        # catastrophic error
+        print(e)
+        return False
+
+
+def shorten_url(url, botan_token, user_id):
+    """
+    Shorten URL for specified user of a bot
+    """
+    try:
+        return requests.get(SHORTENER_URL, params={
+            'token': botan_token,
+            'url': url,
+            'user_ids': str(user_id),
+        }).text
+    except:
+        return url
